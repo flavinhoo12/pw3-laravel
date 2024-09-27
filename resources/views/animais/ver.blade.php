d7c9b2f75ccb5a3271c46a6283ad666852188705
theflash12 <scazzi12@gmail.com>
2024-09-27 11:21:34 -0300

    envio de email com anexo de imagem
    aula 27-09

---
+++ resources/views/animais/ver.blade.php
@@ -0,0 +1,9 @@
+@extends('base')
+
+@section('titulo', 'Animais para adoção - '.$animal->nome)
+
+@section('conteudo')
+    <p><b>Nome: </b> {{ $animal->nome }}</p>
+    <p><b>Idade: </b> {{ $animal->idade }}</p>
+    <p><img src="{{ asset('img/'.$animal->imagem) }}"></p>
+@endsection
--- resources/views/emails/animalcadastrado.blade.php
+++ resources/views/emails/animalcadastrado.blade.php
@@ -1,2 +1,13 @@
 <h1>Animal cadastrado</h1>
-<p>Olha que supimpa! Um novo animal foi cadastrado.</p>
+<p>Olha que supimpa! Um novo animal foi cadastrado</p>
+<p>
+    <b>Nome: </b> {{ $animal->nome }}
+    <br>
+    <b>Idade: </b> {{ $animal->idade }}
+</p>
+<p>Olha a latinha do queridao:
+    <br>
+    <img src="{{ $message->embed('img/'.$animal->imagem) }}">
+</p>
+
+<p>Para saber mais, visite o <a href="{{ route('animais.ver', $animal->id)}}">perfil do animal</a></p>
